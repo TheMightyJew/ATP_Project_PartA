252cbf5c2033d11b23f51b08dd2b75c2822a7167
odedblu <38401370+odedblu@users.noreply.github.com>
2018-04-16 19:03:15 +0300

    generator change

--- ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
+++ ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -2,5 +2,5 @@ package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
     public Maze generate(int rows, int column);
-
+    public long measureAlgorithmTimeMillis(int rows, int column);
 }
