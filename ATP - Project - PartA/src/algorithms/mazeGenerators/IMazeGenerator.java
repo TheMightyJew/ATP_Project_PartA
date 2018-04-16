1db8acafcea10c5c74042194138a7e879f68416c
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2018-04-16 19:07:50 +0300

    defult measuretime function

--- ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
+++ ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -2,5 +2,10 @@ package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
     Maze generate(int rows, int column);
-    long measureAlgorithmTimeMillis(int rows, int column);
+    default long measureAlgorithmTimeMillis(int rows, int columns){
+        long time=System.currentTimeMillis();
+        generate(rows,columns);
+        long ans=time-System.currentTimeMillis();
+        return ans;
+    }
 }
