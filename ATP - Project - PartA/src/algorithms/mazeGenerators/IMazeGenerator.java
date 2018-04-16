5d82a44cde5b46e3475fcffaf5b178d8875fa991
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2018-04-16 18:51:44 +0300

    maze change

--- ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
+++ ATP - Project - PartA/src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -1,4 +1,5 @@
 package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
+
 }
--- ATP - Project - PartA/src/algorithms/mazeGenerators/Maze.java
+++ ATP - Project - PartA/src/algorithms/mazeGenerators/Maze.java
@@ -1,4 +1,16 @@
 package algorithms.mazeGenerators;
+import javafx.util.Pair;
 
 public class Maze {
+    private int[][] array;
+    private int rowsNum;
+    private int columnsNum;
+    private Pair<Integer,Integer> startPoint;
+    private Pair<Integer,Integer> finishPoint;
+    public Maze(int rows,int columns){
+        rowsNum=rows;
+        columnsNum=columns;
+        array=new int[rows][columns];
+        //add generateMaze
+    }
 }
